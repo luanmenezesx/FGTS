20db0d3754d93bf6498782406a46970ff6c7ceff
LuanGCMenezes <luangui2006@gmail.com>
2025-04-05 16:09:59 -0300

    Adicionando Exercício

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+//Quando usado dinheiro usamos DECIMAL
+decimal salario, fgts;
+
+Console.Write("Digite o seu salário: ");
+salario = Convert.ToDecimal(Console.ReadLine()!);
+fgts = salario * 0.08m;
+
+Console.WriteLine($"Salário (R$)..: {salario}");
+Console.WriteLine($"FGTS: R$ {fgts:F2}");
